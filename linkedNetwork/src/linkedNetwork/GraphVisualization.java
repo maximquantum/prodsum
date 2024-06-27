0b12dd634c0e9ca5ef874350abe78d18e61505a8
maximquantum <maximquantum148@gmail.com>
2024-06-27 13:48:27 +0200

    added colors

--- linkedNetwork/src/linkedNetwork/GraphVisualization.java
+++ linkedNetwork/src/linkedNetwork/GraphVisualization.java
@@ -2,11 +2,13 @@ package linkedNetwork;
 
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultUndirectedGraph;
 import com.mxgraph.layout.mxFastOrganicLayout;
 import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.model.mxCell;
 import com.mxgraph.util.mxCellRenderer;
+import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -34,10 +36,10 @@ public class GraphVisualization {
         Set<Rule> rules = new HashSet<>();
         for (int i = 1; i <= limit; i++) {
             for (int j = i+1; j <= limit; j++) {
-            	String first = "P" + String.valueOf(i*j);
-            	String second = "S" + String.valueOf(i+j);
-            	String third = String.format("(%1$s,%2$s)",i,j);
-            	rules.add(new Rule(first, second, third, true));
+                String first = "P" + (i * j);
+                String second = "S" + (i + j);
+                String third = String.format("(%1$s,%2$s)", i, j);
+                rules.add(new Rule(first, second, third, true));
             }
         }
 
@@ -49,19 +51,22 @@ public class GraphVisualization {
         Object parent = mxGraph.getDefaultParent();
 
         Map<String, Object> vertexMap = new HashMap<>();
+        Map<Object, Integer> edgeCountMap = new HashMap<>();
 
         mxGraph.getModel().beginUpdate();
         try {
             for (String node : nodeSet) {
                 Object vertex = mxGraph.insertVertex(parent, null, node, 100, 100, 30, 30);
                 vertexMap.put(node, vertex);
+                edgeCountMap.put(vertex, 0);
             }
 
+            // Add edges to the mxGraph
             for (DefaultEdge edge : graph.edgeSet()) {
                 String source = graph.getEdgeSource(edge);
                 String target = graph.getEdgeTarget(edge);
                 String label = "";
-
+                
                 // Apply labels based on rules
                 for (Rule rule : rules) {
                     if (rule.matches(source, target)) {
@@ -70,7 +75,33 @@ public class GraphVisualization {
                     }
                 }
 
-                mxGraph.insertEdge(parent, null, label, vertexMap.get(source), vertexMap.get(target));
+                Object sourceVertex = vertexMap.get(source);
+                Object targetVertex = vertexMap.get(target);
+                mxGraph.insertEdge(parent, null, label, sourceVertex, targetVertex);
+                edgeCountMap.put(sourceVertex, edgeCountMap.get(sourceVertex) + 1);
+                edgeCountMap.put(targetVertex, edgeCountMap.get(targetVertex) + 1);
+            }
+
+            // Apply colors based on edge counts
+            mxStylesheet stylesheet = mxGraph.getStylesheet();
+            for (Map.Entry<String, Object> entry : vertexMap.entrySet()) {
+                String vertex = entry.getKey();
+                Object cell = entry.getValue();
+                int edgeCount = edgeCountMap.get(cell);
+
+                Map<String, Object> style = new HashMap<>();
+                if (edgeCount == 0) {
+                    style.put(mxConstants.STYLE_FILLCOLOR, "grey");
+                } else if (edgeCount == 1) {
+                    style.put(mxConstants.STYLE_FILLCOLOR, "green");
+                } else if (edgeCount == 2) {
+                    style.put(mxConstants.STYLE_FILLCOLOR, "yellow");
+                } else {
+                    style.put(mxConstants.STYLE_FILLCOLOR, "red");
+                }
+
+                stylesheet.putCellStyle("STYLE_" + vertex, style);
+                ((mxCell) cell).setStyle("STYLE_" + vertex);
             }
         } finally {
             mxGraph.getModel().endUpdate();
@@ -78,6 +109,9 @@ public class GraphVisualization {
 
         // Use a force-directed layout to position the nodes
         mxIGraphLayout layout = new mxFastOrganicLayout(mxGraph);
+        ((mxFastOrganicLayout) layout).setForceConstant(100); // Further increase the force constant to reduce overlapping
+        ((mxFastOrganicLayout) layout).setInitialTemp(200); // Increase the initial temperature for better spreading
+        ((mxFastOrganicLayout) layout).setMinDistanceLimit(100); // Increase the minimum distance between nodes
         layout.execute(parent);
 
         // Export the graph as an image
@@ -87,8 +121,6 @@ public class GraphVisualization {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-       
     }
     
     private static Set<String> generateNodes(int minP, int maxP, int minS, int maxS) {
